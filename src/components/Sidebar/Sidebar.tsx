ca49c94f413cabd0579dac1c0d254e8813de2c15
Alex <aironer@yandex.ru>
2025-09-26 13:01:57 +0300

    Рефакторинг кода

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,35 +7,31 @@ function Sidebar() {
   const open = useSelector(openMenuSelector);
   const dispatch = useDispatch();
 
-  const closeMenu = () => {
-    dispatch(setSidebarOpen(false));
-  };
+  const closeMenu = () => dispatch(setSidebarOpen(false));
+
+  const categories = [
+    "SCIENCE",
+    "GENERAL",
+    "ENTERTAINMENT",
+    "TECHNOLOGY",
+    "BUSINESS",
+    "HEALTH",
+    "SPORTS",
+  ];
 
   return (
     <div className={`sidebar-overlay ${open ? "sidebar-open" : ""}`}>
       <aside
         className={`sidebar ${open ? "sidebar--open" : ""}`}
-        aria-hidden={!open}
+        tabIndex={open ? 0 : -1}
       >
-        <button
-          className="sidebar__close"
-          aria-label="Close menu"
-          onClick={closeMenu}
-        >
+        <button className="sidebar__close" onClick={closeMenu}>
           ×
         </button>
         <ul className="sidebar__list">
-          {[
-            "SCIENCE",
-            "GENERAL",
-            "ENTERTAINMENT",
-            "TECHNOLOGY",
-            "BUSINESS",
-            "HEALTH",
-            "SPORTS",
-          ].map((cat) => (
-            <li key={cat} className="sidebar__item">
-              {cat}
+          {categories.map((category) => (
+            <li key={category} className="sidebar__item">
+              {category}
             </li>
           ))}
         </ul>
